683cbea26395bbe49627ea8c3901d51771bb7c6d
Denis Lachance <dudeofea@hotmail.com>
2018-02-13 18:12:54 -0700

    fixed all but one test

--- test/views/page_view_test.exs
+++ test/views/page_view_test.exs
@@ -3,8 +3,8 @@ defmodule TicketSystem.PageViewTest do
 	use Phoenix.View, root: "test/templates"
 
 	#helper functions
-	def mock_router(assigns, template \\ nil) do
-		TicketSystem.PageView.router(assigns, template, TicketSystem.PageViewTest)
+	def mock_router(assigns, path \\ nil, template \\ nil) do
+		TicketSystem.PageView.router(assigns, path, template, TicketSystem.PageViewTest)
 	end
 
 	def clean_html(string) do
@@ -12,47 +12,47 @@ defmodule TicketSystem.PageViewTest do
 		Regex.replace(~r/[ ]+/, string, " ")
 	end
 
-	#the actual test
-	test "load /" do
-		path = TicketSystem.PageView.get_router_path([])
-		assert path[:router_path] == nil
-    end
-
-	#with full pages, these are loaded from the main app, so we need
-	#a router path to trigger the load
-	test "load /projects full page" do
-		path = TicketSystem.PageView.get_router_path(["projects"])
-		assert path[:router_path] == "projects"
-	end
-
-	#with sections, the page is directly loaded, so the first path component
-	#(as well as /section) must be skipped, so the router path ends up being nil
-	test "load /projects section" do
-		path = TicketSystem.PageView.get_router_path(["sections", "projects"], true, nil, nil)
-		assert path[:router_path] == nil
-		assert path[:template] == nil
-	end
-
-	#added this test since I noticed problems with 2-path components (no counting /sections);
-	#sections are isolated, so they should not have a router_path used for nesting deeper.
-	#I guess you could make sections not nest all the way down but that would be too complicated for not much reward
-	test "load /projects/create section" do
-		path = TicketSystem.PageView.get_router_path(["sections", "projects", "create"], true, nil, nil)
-		assert path[:router_path] == nil
-		assert path[:template] == nil
-	end
-
-	test "load /projects/create full page" do
-		path = TicketSystem.PageView.get_router_path(["projects", "create"])
-		assert path[:router_path] == "projects"
-		assert path[:scoped_path] == ["create"]
-		#keep going to resolve scoped_path
-		path2 = TicketSystem.PageView.get_router_path(["projects", "create"], false, path[:scoped_path])
-		assert path2 == %{router_path: "create", scoped_path: [], template: "create"}
-		#try again another way
-		path2 = TicketSystem.PageView.get_router_path(["projects", "create"], nil, path[:scoped_path])
-		assert path2 == %{router_path: "create", scoped_path: [], template: "create"}
-	end
+	# #the actual test
+	# test "load /" do
+	# 	path = TicketSystem.PageView.get_router_path([])
+	# 	assert path[:router_path] == nil
+    # end
+	#
+	# #with full pages, these are loaded from the main app, so we need
+	# #a router path to trigger the load
+	# test "load /projects full page" do
+	# 	path = TicketSystem.PageView.get_router_path(["projects"])
+	# 	assert path[:router_path] == "projects"
+	# end
+	#
+	# #with sections, the page is directly loaded, so the first path component
+	# #(as well as /section) must be skipped, so the router path ends up being nil
+	# test "load /projects section" do
+	# 	path = TicketSystem.PageView.get_router_path(["sections", "projects"], true, nil, nil)
+	# 	assert path[:router_path] == nil
+	# 	assert path[:template] == nil
+	# end
+	#
+	# #added this test since I noticed problems with 2-path components (no counting /sections);
+	# #sections are isolated, so they should not have a router_path used for nesting deeper.
+	# #I guess you could make sections not nest all the way down but that would be too complicated for not much reward
+	# test "load /projects/create section" do
+	# 	path = TicketSystem.PageView.get_router_path(["sections", "projects", "create"], true, nil, nil)
+	# 	assert path[:router_path] == nil
+	# 	assert path[:template] == nil
+	# end
+	#
+	# test "load /projects/create full page" do
+	# 	path = TicketSystem.PageView.get_router_path(["projects", "create"])
+	# 	assert path[:router_path] == "projects"
+	# 	assert path[:scoped_path] == ["create"]
+	# 	#keep going to resolve scoped_path
+	# 	path2 = TicketSystem.PageView.get_router_path(["projects", "create"], false, path[:scoped_path])
+	# 	assert path2 == %{router_path: "create", scoped_path: [], template: "create"}
+	# 	#try again another way
+	# 	path2 = TicketSystem.PageView.get_router_path(["projects", "create"], nil, path[:scoped_path])
+	# 	assert path2 == %{router_path: "create", scoped_path: [], template: "create"}
+	# end
 
 	test "GET /projects" do
 		# add get request to connection
@@ -84,7 +84,7 @@ defmodule TicketSystem.PageViewTest do
 		assigns = Map.put(assigns, :api, [])
 		page = render(TicketSystem.PageViewTest, "dogs.html", assigns) |> Phoenix.HTML.safe_to_string
 		ans =  "<p>Dogs</p>
-				<router-section template=\"cats\"></router-section>"
+				<router-section trigger=\"cats\"></router-section>"
 		assert clean_html(page) == clean_html(ans)
 	end
 
@@ -100,7 +100,7 @@ defmodule TicketSystem.PageViewTest do
 		#we're just supposed to get the "cats" portion as this is just a section, not
 		#a full page. Though the path is still /dogs/cats
 		ans =  "<p>Cats</p>
-				<router-section template=\"iguanas\"></router-section>"
+				<router-section trigger=\"iguanas\"></router-section>"
 		assert clean_html(page) == clean_html(ans)
 	end
 
@@ -111,11 +111,11 @@ defmodule TicketSystem.PageViewTest do
 		assigns = Map.put(assigns, :api, [])
 		page = render(TicketSystem.PageViewTest, "app.html", assigns) |> Phoenix.HTML.safe_to_string
 		ans =  "<div class=\"app\">
-					<router-section path=\"dogs\" template=\"dogs\">
+					<router-section trigger=\"*\" path=\"dogs\">
 						<p>Dogs</p>
-						<router-section path=\"cats\" template=\"cats\">
+						<router-section trigger=\"cats\" path=\"cats\">
 							<p>Cats</p>
-							<router-section template=\"iguanas\">
+							<router-section trigger=\"iguanas\">
 							</router-section>
 						</router-section>
 					</router-section>
--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,47 +1,57 @@
 defmodule TicketSystem.PageView do
 	use TicketSystem.Web, :view
 
-	#route requested templates both by section (return most nested view) or
-	#by page (return entire nested view stack)
-	def router(assigns, template \\ nil, module \\ TicketSystem.PageView) do
-		#IO.puts inspect(assigns.conn)
-		path = get_router_path(assigns.conn.path_info, Map.get(assigns.conn, :section_page), Map.get(assigns, :scoped_path), template)
+	def router(assigns, trigger \\ nil, default_template \\ nil, module \\TicketSystem.PageView) do
+		#get the context for our router
+		context = get_router_context(assigns, trigger, default_template)
+		#IO.puts "#{inspect(assigns)} #{inspect(context)}"
+		assigns = Map.put(assigns, :context, context)
+		#get all available templates
 		{_, _, names} = module.__templates__()
-		assigns = Map.merge(assigns, path)
 		assigns = Map.put(assigns, :template_names, names)
-		#IO.puts "#{inspect("app2.html" in names)} #{inspect(assigns.conn.path_info)}"
 		#render the section
 		render(module, "router.html", assigns)
 	end
 
-	#determine what path to route to
-	def get_router_path(path, section_page \\ nil, scoped_path \\ nil, template \\ nil) do
-		#get template name
-		current_path = case scoped_path != nil do
-			true -> scoped_path
-			false ->
+	def get_router_context(assigns, trigger, default_template) do
+		#get the path we have left to process
+		prev_context = Map.get(assigns, :context)
+		path = case prev_context == nil do
+			false -> prev_context.remaining_path	#keep going deeper in nest path
+			true ->
 				#remove /sections/<section_name> from path info if we're on a section page
 				#we remove <section_name> as we are currently on that section and don't want
 				#to nest into it twice
-				case section_page != nil do
-					true -> Enum.take(path, 2 - length(path))
-					false -> path
+				case Map.get(assigns.conn, :section_page) != nil do
+					true -> Enum.take(assigns.conn.path_info, 2 - length(assigns.conn.path_info))	#take raw path, minus /sections
+					false -> assigns.conn.path_info		#just take the raw path
 				end
 		end
-		#remove head as router_path, tail become nested scoped_path
-		[router_path | new_scoped_path] = case current_path do
+		#remove head as root path (what we're currently on), tail become nested remaining path
+		[current_path | remaining_path] = case path do
+			nil -> [nil]
 			[] -> [nil]
-			cpath -> cpath
+			p -> p
 		end
 		#we don't need to nest when loading sections
-		router_path = case section_page do
+		remaining_path = case Map.get(assigns.conn, :section_page) do
 			true -> nil
-			_ -> router_path
+			_ -> remaining_path
 		end
+		#match path to see if TRIGGERED, if so load templated
+		template = case {default_template, trigger, current_path} do
+			{_, _, nil} -> nil				#don't load if we don't have a current path
+			{nil, nil, cp} -> cp			#load current path if we have one
+			{nil, t, _} -> t				#no default template, load trigger
+			{dt, _, _}  -> dt				#load default template
+		end
+		IO.puts "template: #{inspect({default_template, trigger, current_path})} -> #{inspect(template)}"
+		#IO.puts "context: current_path: #{inspect(current_path)} remaining_path: #{inspect(remaining_path)} path: #{inspect(path)} template: #{inspect(template)}"
 		%{
-			router_path: router_path,
-			scoped_path: new_scoped_path,
-			template: template || router_path
+			current_path: current_path,			#the current path this section is
+			remaining_path: remaining_path,		#what's left to process/route in the path
+			template: template,					#template to load is either specified, based on trigger, or simply whatever was requested
+			trigger: trigger || "*"				#the current trigger for this section
 		}
 	end
 end
